bc77d13c7d486cbde6774b6bfcc4be55116b85a2
MOHD SHABAZ <MOHD SHABAZ>
2017-03-26 09:56:18 +0530

    First commit

---
+++ goli/src/components/Hello.vue
@@ -0,0 +1,57 @@
+<template>
+
+    <div class="Hello">
+    
+        <h1>IDEAS</h1>
+
+        <form v-on:submit="addIdea">
+
+            <input type="text" v-model="newIdea.note" placeholder="Start your idea!!!">
+            <br />
+            <input type="submit" value="Submit">
+
+        </form>
+
+        <ul>
+            <li v-for="idea in ideas"> 
+                    {{idea.note}} <button v-on:click="deleteIdea(idea)">x</button>
+            </li>
+        </ul>
+
+    </div>
+
+</template>
+
+<script>
+    export default{
+        name: 'ideas',
+
+        data() { 
+            return {
+                newIdea: {},
+                ideas: [   /*
+                    {
+                    note:'Type Here'
+                    }  */
+                ]   
+            }
+        },
+        methods: {
+            addIdea: function(e){
+                this.ideas.push({
+                    note: this.newIdea.note,
+                }); 
+                this.newIdea = {};
+                e.preventDefault();
+            },
+            deleteIdea: function(idea){
+                this.ideas.splice(this.ideas.indexOf(idea),1);
+            }
+        },
+    }
+</script>   
+
+
+<style scoped>
+
+</style>
